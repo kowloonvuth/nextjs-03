4ac77a4c6967e1ad37674a087af681eeaf4fbaf6
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-09-05 03:22:15 +0700

    nextjs: SSR, SSG, ISR

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,8 +1,11 @@
 import getUser from "@/lib/getUser"
 import getUserPosts from "@/lib/getUserPost"
 import UserPosts from "./component/UserPosts"
+import getAllUsers from "@/lib/getAllUsers"
 import { Suspense } from "react"
 import type { Metadata } from "next"
+
+import { notFound } from "next/navigation"
 
 type Params = {
     params: {
@@ -13,6 +16,12 @@ type Params = {
 export async function generateMetadata({ params: {userId}}: Params) : Promise<Metadata> {
     const userData: Promise<User> = getUser(userId)
     const user: User = await userData
+
+    if(!user.name) {
+        return {
+            title: "User Not Found"
+        }
+    }
 
     return {
         title: user.name,
@@ -27,6 +36,8 @@ export default async function UserPage({ params: { userId }} : Params) {
     //const [user, userPosts] = await Promise.all([userData, userPostsData])
 
     const user = await userData
+
+    if (!user.name) return notFound()
   return (
     <>
         <h2>{user.name}</h2>
@@ -36,4 +47,13 @@ export default async function UserPage({ params: { userId }} : Params) {
         </Suspense>
     </>
   )
+}
+
+export async function generateStaticParams() {
+    const usersData: Promise<User[]> = getAllUsers()
+    const users = await usersData
+
+    return users.map(user => (
+        { userId: user.id.toString() }
+    ))
 }
--- lib/getUserPost.tsx
+++ lib/getUserPost.tsx
@@ -1,7 +1,7 @@
 export default async function getUserPosts(userId: string) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
 
-    if (!res.ok) throw Error('failed to fetch user')
+    if (!res.ok) return undefined
   
     return res.json()
 }
